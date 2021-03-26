abbca1fe92377802462d06dc87255a50404738c8
sunbee <sanjay.bhatikar@gmail.com>
2021-03-26 11:28:30 -0700

    Refactored code to loop over data-bindings.

--- app.js
+++ app.js
@@ -11,37 +11,35 @@ Use a timeout function to see step-step changes.
 */
 document.addEventListener('DOMContentLoaded', () => {
 
-	const number_first = new Observable(3);
-	const number_one = document.getElementById("number_one");
-	bind2DOM(number_one, number_first);
-
-	const number_second = new Observable(4);
-	const number_two = document.getElementById("number_two");
-	bind2DOM(number_two, number_second)
-
-	const result_summa = new Computed(() => Number(number_first.value) + Number(number_second.value),
+	var number_first = new Observable(3);
+	var number_second = new Observable(4);
+	var summa = new Computed(() => Number(number_first.value) + Number(number_second.value),
 						[number_first, number_second]);
-	const result_out = document.getElementById("summa");
-	bind2DOM(summa, result_summa);
-
-	const name = new Observable("Lal");
-	const name_first = document.getElementById("name_first");
-	bind2DOM(name_first, name);
-
-	const surname = new Observable("Bahadur");
-	const name_second = document.getElementById("name_second");
-	bind2DOM(name_second, surname);
-
-	const full_name = new Computed(() => `${name.value} ${surname.value}`, [name, surname]);
-	const answer = document.getElementById("name_full");
-	bind2DOM(answer, full_name);
-
-	const outcome = new Computed(() => { 
-		if (surname.value == 'Shastri') {
+	var name_first = new Observable("Lal");
+	var name_last = new Observable("Bahadur");
+	var name_full = new Computed(() => `${name_first.value} ${name_last.value}`, [name_first, name_last]);
+	var score = new Computed(() => { 
+		if (name_last.value == 'Shastri') {
 			return "Bravo! You win!"
 		} else {
 			return "Keep trying!"
-		}}, [surname]);
-	const score = document.getElementById("score");
-	bind2DOM(score, outcome);
+		}}, [name_last]);
+
+	var context = {
+		number_one: number_first,
+		number_two: number_second,
+		summa: summa,
+		given_name: name_first,
+		surname: name_last,
+		fullname: name_full,
+		score: score 
+	};
+
+	var dataBindings = document.querySelectorAll("[data-binding]");
+	dataBindings.forEach(inputElement => {
+		console.log(inputElement);
+
+		bind2DOM(inputElement, context[inputElement.getAttribute("data-binding")])
+	});
+
 });
